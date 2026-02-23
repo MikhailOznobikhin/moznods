1a5e9a9599d31b387c9e79d9234c9dcb5f73e886
m_ozno <m.oznobikhin@fizikl.org>
2026-02-23 13:07:08 +0800

    fix: docker and celery

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,8 +11,42 @@ from .base import *  # noqa: F401, F403, F405
 load_dotenv(BASE_DIR / ".env")  # noqa: F405
 
 DEBUG = True
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "0.0.0.0", "*"]
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]"]
+# Add logging to see full tracebacks in console
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": True,
+        },
+        "django.request": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+        "gunicorn.error": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-dev-key")
 
